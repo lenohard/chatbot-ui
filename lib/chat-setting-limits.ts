d5893cdda8f70fb015a1b32865b10b044345435a
GaussEuler <mathestics@gmail.com>
2024-07-13 13:21:20 +0800

    hot fix

--- lib/chat-setting-limits.ts
+++ lib/chat-setting-limits.ts
@@ -60,12 +60,6 @@ export const CHAT_SETTING_LIMITS: Record<LLMID, ChatSettingLimits> = {
     MAX_TOKEN_OUTPUT_LENGTH: 8192,
     MAX_CONTEXT_LENGTH: 1040384
   },
-  "gemini-1.5-flash-latest": {
-    MIN_TEMPERATURE: 0.0,
-    MAX_TEMPERATURE: 1.0,
-    MAX_TOKEN_OUTPUT_LENGTH: 8192,
-    MAX_CONTEXT_LENGTH: 1040384
-  },
   "gemini-pro": {
     MIN_TEMPERATURE: 0.0,
     MAX_TEMPERATURE: 1.0,
